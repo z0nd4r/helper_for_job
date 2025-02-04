b009ecd68cde0168afd9dd437b71151b53f222c7
Sergey <kirito98730tkm@gmail.com>
2025-02-04 22:57:55 +0200

    Delete app/Backend directory

--- app/Backend/api/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, PydanticUserError
-from uuid import UUID
-
-class TaskModel(BaseModel):
-        id: UUID
-        title: str = Field(max_length=30)
-        description: str = Field(max_length=150)
-        status: str
