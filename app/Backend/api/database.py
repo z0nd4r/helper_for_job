46de641a8ca5e11aed57f35d1aecb8becca36a99
Zondar__ <kirito98730tkm@gmail.com>
2025-02-09 01:29:56 +0200

    Добавлена удаленная бд

--- app/Backend/api/database.py
+++ app/Backend/api/database.py
@@ -1,12 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:mirexdoomcrfqhbv199@localhost:5432/crm"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_USER = "postgres.zyfosgbvyefnreuglbdy" # Usually postgres
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = "aws-0-us-west-1.pooler.supabase.com"
+DATABASE_PORT = "5432"
+DATABASE_NAME = "postgres"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+
+try:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+except OperationalError as e:
+    print(f"Error connecting to database: {e}")
 
 Base = declarative_base()
 
--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from fastapi import FastAPI, Depends
 import uvicorn
 from app.Backend.api.database import engine, Base
 from app.Backend.api.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
+
+# Определите домены, с которых разрешены запросы
+origins = [
+    # "http://localhost:3000",  # Например, для локальной разработки React
+    "https://qsoops.github.io", # Замените на домен вашего фронтенда
+    # "https://your-frontend-domain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Разрешить передачу куки (если требуется)
+    allow_methods=["*"],      # Разрешить все HTTP методы (GET, POST, PUT, DELETE, ...)
+    allow_headers=["*"],      # Разрешить все заголовки
+)
 
 Base.metadata.create_all(bind=engine)
 
